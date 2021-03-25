cbd6dd4bf6fd4a83665640cc8d91826fdbc2d005
saiak <akkenapallysai45@gmail.com>
2021-03-25 20:45:51 +0530

    ds

--- fix/fix/settings.py
+++ fix/fix/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8m4w_xsf$9ud%d&+fyu80suv#*ly*#rv-qw)nxm!z46)^f1wpt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fixre.herokuapp.com','localhost']
 
